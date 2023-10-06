b4546611d246475fff45d5b4e169ba782f418fee
nikushasavior <146087709+nikushasavior@users.noreply.github.com>
2023-10-06 13:40:44 +0400

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,94 +1,13 @@
-from turtle import *
-
-#we want to paint a house
-
-#step 1:  draw a square
-speed(30)
-width(7)
-color("blue")
-forward(200)
-left(90)
-
-forward(200)
-left(90) 
-
-forward(200)
-left(90)
-
-forward(200)
-left(90)
-#end of square
-
-#drawing a door
-
-forward(70)
-color("red")
-left(90)
-forward(120)  #height of the door
-right(90)
-forward(60)
-right(90)
-forward(120)
-
-penup()
-goto(200, 200)
-pendown()
-
-color("green")
-begin_fill()
-right(150)
-forward(200)
-left(120)
-forward(200)
-end_fill()
-
-
-
-exitonclick()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(deda "37")
+print(mama "38")
+print(bebia "66")
+print(da "15")
+print (bidza "40")
+print(deida"40")
+print(babua "61")
+print(bidzashvili "16")
+print(me "14")
+print("zestafonis qucha")
+print(sadarbazo "1")
+print(bina "16")
+print(sartuli "1")
